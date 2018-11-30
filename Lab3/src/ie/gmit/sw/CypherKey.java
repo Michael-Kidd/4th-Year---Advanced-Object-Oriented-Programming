899ce2a5ca0788bfbe766978d67e2b84a8da9fda
Michael Kidd <G00236920@gmit.ie>
2018-11-30 22:32:42 +0000

    Removed labs from repo

--- Lab3/src/ie/gmit/sw/CypherKey.java
+++
@@ -1,6 +0,0 @@
-package ie.gmit.sw;
-
-public interface CypherKey {
-	public void setKey(String key) throws CypherException;
-	public String getKey();
-}
--- Lab4/src/ie/gmit/sw/CypherType.java
+++
@@ -1,7 +0,0 @@
-package ie.gmit.sw;
-
-public enum CypherType {
-	
-	DES, AES, RSA, TripleDES, Vigenere, EllipticCurve;
-	
-}
